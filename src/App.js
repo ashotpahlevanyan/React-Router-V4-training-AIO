838756cb5750ecc45ed18e7594e9c43cd7c34bcd
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-17 01:12:19 +0400

    redirect with Switch

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Link,
+	Redirect,
+	Switch,
 } from 'react-router-dom';
 
 import './App.css';
 
-const Home = () => <h1>Home</h1>;
-const Menu = () => (
-	<div>
-		<h1>Menu</h1>
-		<Link to="/menu/drink">Drinks</Link>
-		<Link to="/menu/food">Foods</Link>
-		<Link to="/menu/sides">sides</Link>
-		<Route path="/menu/:section"
-		       render={({match}) => <h2>{match.params.section}</h2>} />
-	</div>
+const Links = () => (
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/old">Old</Link>
+		<Link to="/new">New</Link>
+	</nav>
 );
 
 const App = (props) => (
 	<Router>
 		<div>
-			<Link to="/home">Home</Link>
-			<Link to="/menu">Menu</Link>
-			<Route exact path="/" component={Home}/>
-			<Route path="/menu" component={Menu}/>
+			<Links />
+			<Switch>
+				<Route exact path="/" render={() => (<h1>Home</h1>)}/>
+				<Route path="/new" render={() => (<h1>New</h1>)}/>
+				<Redirect from="/old" to="/new" />
+			</Switch>
 		</div>
 	</Router>
 );
