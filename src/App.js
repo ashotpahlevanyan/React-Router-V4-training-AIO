0ba2323be1e7f6bed40b1a820a5050c158e100b3
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-17 01:26:20 +0400

    protected login redirection

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import {
 } from 'react-router-dom';
 
 import './App.css';
+const loggedin = false;
 
 const Links = () => (
 	<nav>
 		<Link to="/">Home</Link>
-		<Link to="/old">Old</Link>
-		<Link to="/new">New</Link>
+		<Link to="/old/123">Old</Link>
+		<Link to="/new/456">New</Link>
+		<Link to="/protected">Protected</Link>
 	</nav>
 );
 
@@ -21,11 +23,23 @@ const App = (props) => (
 	<Router>
 		<div>
 			<Links />
-			<Switch>
-				<Route exact path="/" render={() => (<h1>Home</h1>)}/>
-				<Route path="/new" render={() => (<h1>New</h1>)}/>
-				<Redirect from="/old" to="/new" />
-			</Switch>
+			<Route exact path="/" render={() => (<h1>Home</h1>)}/>
+			<Route path="/new/:str"
+	       render={({match}) =>
+		       (<h1>New : {match.params.str}</h1>)}/>
+			<Route path="/old/:str"
+	       render={({match}) => (
+	        <Redirect to={`/new/${match.params.str}`} />
+	       )}
+			/>
+
+			<Route path="/protected"
+				render={() => (
+				 loggedin
+				   ? <h1>Welcome to Protected page</h1>
+				   : <Redirect to={'/new/Login'} />
+				)}
+		/>
 		</div>
 	</Router>
 );
