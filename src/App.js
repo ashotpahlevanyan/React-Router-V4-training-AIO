c2505175e1668cd82727203dce470f6c85364e10
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-17 01:30:57 +0400

    initial state for intercept route changes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Link,
-	Redirect,
-	Switch,
 } from 'react-router-dom';
 
 import './App.css';
-const loggedin = false;
+
+const Home = () => (<h1>Home</h1>);
+const Form = () => (<h1>Form</h1>);
+
 
 const Links = () => (
 	<nav>
 		<Link to="/">Home</Link>
-		<Link to="/old/123">Old</Link>
-		<Link to="/new/456">New</Link>
-		<Link to="/protected">Protected</Link>
+		<Link to="/form">Form</Link>
 	</nav>
 );
 
@@ -23,23 +22,8 @@ const App = (props) => (
 	<Router>
 		<div>
 			<Links />
-			<Route exact path="/" render={() => (<h1>Home</h1>)}/>
-			<Route path="/new/:str"
-	       render={({match}) =>
-		       (<h1>New : {match.params.str}</h1>)}/>
-			<Route path="/old/:str"
-	       render={({match}) => (
-	        <Redirect to={`/new/${match.params.str}`} />
-	       )}
-			/>
-
-			<Route path="/protected"
-				render={() => (
-				 loggedin
-				   ? <h1>Welcome to Protected page</h1>
-				   : <Redirect to={'/new/Login'} />
-				)}
-		/>
+			<Route exact path="/" component={Home} />
+			<Route path="/form" component={Form} />
 		</div>
 	</Router>
 );
