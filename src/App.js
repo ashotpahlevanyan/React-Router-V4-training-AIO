3e344e7777897f83f433aab557c299e609fba581
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-17 00:31:33 +0400

    added header and content, they also can be added inside Links instead and it will work as expected

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Link,
-	Switch,
 } from 'react-router-dom';
 
 import './App.css';
 
-const Home = (props) => {
-	console.log(props);
-	return <h1>Home</h1>;
-};
-
 const Links = () => (
 	<nav>
-		<Link to="/">Home</Link>
+		<Link to="/home">Home</Link>
 		<Link to="/about">About</Link>
-		<Link to="/contact">Contact</Link>
 	</nav>
+);
+
+const Header = ({match}) => (
+	<div className="header">
+		<Route path="/:page"
+			render={({match}) => (
+			<h1>{match.params.page} header</h1>)}
+		/>
+	</div>
+);
+
+const Content = ({match}) => (
+	<div className="content">
+		<Route path="/:page"
+			render={({match}) => (
+			<h1>{match.params.page} content</h1>)}
+		/>
+	</div>
 );
 
 class App extends React.Component {
@@ -27,19 +38,12 @@ class App extends React.Component {
 			<Router>
 				<div>
 					<Links />
-					<Switch>
-						<Route exact path="/" render={() => <h1>Home</h1>} />
-						<Route path="/about" render={() => <h1>About</h1>} />
-						<Route path="/contact" render={() => <h1>Contact</h1>} />
-						<Route
-							path="/:itemId"
-							render={({match}) => (<h1>Item: {match.params.itemId}</h1>)} />
-					</Switch>
+					<Content />
+					<Header />
 				</div>
 			</Router>
 		);
 	}
 }
-
 
 export default App;
